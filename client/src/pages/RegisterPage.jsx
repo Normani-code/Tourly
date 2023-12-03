84509de3c7adbf5d6e39b0211a3eb54274b448aa
Norman <63623125+Normani-code@users.noreply.github.com>
2023-12-03 14:27:35 -0500

    Update RegisterPage.jsx

--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -22,23 +22,23 @@ export default function RegisterPage() {
   return (
     <div className="mt-4 grow flex items-center justify-around">
       <div className="mb-64">
-        <h1 className="text-4xl text-center mb-4">Register</h1>
+        <h1 className="text-4xl text-center mb-4">Registro</h1>
         <form className="max-w-md mx-auto" onSubmit={registerUser}>
           <input type="text"
-                 placeholder="John Doe"
+                 placeholder="Nombre"
                  value={name}
                  onChange={ev => setName(ev.target.value)} />
           <input type="email"
-                 placeholder="your@email.com"
+                 placeholder="Ejemplo@email.com"
                  value={email}
                  onChange={ev => setEmail(ev.target.value)} />
           <input type="password"
-                 placeholder="password"
+                 placeholder="Contraseña"
                  value={password}
                  onChange={ev => setPassword(ev.target.value)} />
-          <button className="primary">Register</button>
+          <button className="primary">Registrar</button>
           <div className="text-center py-2 text-gray-500">
-            Already a member? <Link className="underline text-black" to={'/login'}>Login</Link>
+           🌴Ya eres un Touly lover? <Link className="underline text-black" to={'/login'}>Iniciar Sesión</Link>
           </div>
         </form>
       </div>
