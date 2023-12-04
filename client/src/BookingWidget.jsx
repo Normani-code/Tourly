6d80c61bac9ba6f694b9699d586136abed165403
Norman <63623125+Normani-code@users.noreply.github.com>
2023-12-04 15:03:45 -0500

    Update BookingWidget.jsx

--- client/src/BookingWidget.jsx
+++ client/src/BookingWidget.jsx
@@ -26,9 +26,9 @@ export default function BookingWidget({place}) {
 
   async function bookThisPlace() {
     const response = await axios.post('/bookings', {
-      checkIn,checkOut,numberOfGuests,name,phone,
+    numberOfGuests,
       place:place._id,
-      price:numberOfNights * place.price,
+      price:numberOfGuests * place.price,
     });
     const bookingId = response.data._id;
     setRedirect(`/account/bookings/${bookingId}`);
@@ -41,45 +41,20 @@ export default function BookingWidget({place}) {
   return (
     <div className="bg-white shadow p-4 rounded-2xl">
       <div className="text-2xl text-center">
-        Price: ${place.price} / per night
+        Price: ${place.price} / por persona
       </div>
       <div className="border rounded-2xl mt-4">
-        <div className="flex">
-          <div className="py-3 px-4">
-            <label>Check in:</label>
-            <input type="date"
-                   value={checkIn}
-                   onChange={ev => setCheckIn(ev.target.value)}/>
-          </div>
-          <div className="py-3 px-4 border-l">
-            <label>Check out:</label>
-            <input type="date" value={checkOut}
-                   onChange={ev => setCheckOut(ev.target.value)}/>
-          </div>
-        </div>
         <div className="py-3 px-4 border-t">
           <label>Number of guests:</label>
           <input type="number"
                  value={numberOfGuests}
                  onChange={ev => setNumberOfGuests(ev.target.value)}/>
         </div>
-        {numberOfNights > 0 && (
-          <div className="py-3 px-4 border-t">
-            <label>Your full name:</label>
-            <input type="text"
-                   value={name}
-                   onChange={ev => setName(ev.target.value)}/>
-            <label>Phone number:</label>
-            <input type="tel"
-                   value={phone}
-                   onChange={ev => setPhone(ev.target.value)}/>
-          </div>
-        )}
       </div>
       <button onClick={bookThisPlace} className="primary mt-4">
-        Book this place
-        {numberOfNights > 0 && (
-          <span> ${numberOfNights * place.price}</span>
+        Contactame 📞
+        {numberOfGuests > 0 && (
+          <span> ${numberOfGuests * place.price}</span>
         )}
       </button>
     </div>
