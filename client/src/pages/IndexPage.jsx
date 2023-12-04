bd41e6587a857ca70a4a7bad2bb6ab31fee09952
Norman <63623125+Normani-code@users.noreply.github.com>
2023-12-04 14:28:57 -0500

    Update IndexPage.jsx

--- client/src/pages/IndexPage.jsx
+++ client/src/pages/IndexPage.jsx
@@ -18,11 +18,11 @@ export default function IndexPage() {
             {place.photos?.[0] && (
               <Image className="rounded-2xl object-cover aspect-square" src={place.photos?.[0]} alt=""/>
             )}
-          </div>
-          <h2 className="font-bold">{place.address}</h2>
-          <h3 className="text-sm text-gray-500">{place.title}</h3>
+          </div> 
+          <h2 className="font-bold">{place.title}</h2>
+          <h3 className="text-sm text-gray-500">{place.address}</h3>
           <div className="mt-1">
-            <span className="font-bold">${place.price}</span> per night
+            <span className="font-bold">${place.price}</span> por persona
           </div>
         </Link>
       ))}
